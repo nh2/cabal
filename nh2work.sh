0629e13f4931b34572bccab43d2c27fa6a1d599e
Niklas Hambüchen <mail@nh2.me>
2013-10-12 16:46:02 +0100

    Update nh2work script

--- nh2work.sh
+++ nh2work.sh
@@ -5,4 +5,6 @@ echo 'Creating nh2work branch'
 
 git checkout nh2work
 git reset --hard build-executables-in-parallel
-git merge avoid-linking profiling-in-parallel flush-stdout dont-touch-files fix-best-version
+git merge avoid-linking dont-touch-files
+# fix-best-version       only for development, maybe already merged
+# profiling-in-parallel  unfortunately not correct due to https://github.com/haskell/cabal/pull/1413
